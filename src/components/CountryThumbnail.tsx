0388ba12fbfcb2f3729199f07fd64539277263c8
Patricija Delac <patricija.delac@agiledrop.com>
2025-02-24 12:31:50 +0100

    Refactor CountryThumbnail component

--- src/components/CountryThumbnail.tsx
+++ src/components/CountryThumbnail.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { CountryProps } from '../types';
 import { formatNumber } from '../utils/format';
+import DetailList from './DetailList';
 
 interface CountryThumbnailProps {
   country: CountryProps;
@@ -10,6 +11,18 @@ interface CountryThumbnailProps {
 export default function CountryThumbnail({ country }: CountryThumbnailProps) {
   const { name, flags, population, region, capital } = country;
   const linkRef = useRef<HTMLAnchorElement>(null);
+
+  const countryDetails = [
+    {
+      label: 'Population',
+      value: formatNumber(population),
+    },
+    { label: 'Region', value: region },
+    {
+      label: 'Capital',
+      value: capital?.join(', '),
+    },
+  ];
 
   const handleClick = () => {
     linkRef.current?.click();
@@ -32,14 +45,15 @@ export default function CountryThumbnail({ country }: CountryThumbnailProps) {
       onClick={handleClick}
       onKeyDown={(event) => handleKeyDown(event)}
       aria-label={`Go to details of ${name.common}`}
-      className="bg-white rounded-[5px] overflow-hidden cursor-pointer dark:bg-dark-blue"
+      className="bg-white rounded-[5px] overflow-hidden shadow-custom cursor-pointer dark:bg-dark-blue"
     >
       <img
         src={flags.svg}
         alt={flags.alt || `Flag of ${name.common}`}
         className="w-full h-[160px] object-cover"
       />
-      <div className="flex flex-col gap-2 p-6 text-sm leading-4 font-light">
+
+      <div className="flex flex-col gap-2 p-6 pb-[46px] text-sm leading-6 font-light">
         <h2 className="text-lg leading-[26px] font-extrabold">
           <Link
             to={`/country/${name.common.toLowerCase().replaceAll(' ', '-')}`}
@@ -50,29 +64,9 @@ export default function CountryThumbnail({ country }: CountryThumbnailProps) {
             {name.common}
           </Link>
         </h2>
-        <dl className="flex flex-col gap-2">
-          <DescriptionItem
-            label="Population"
-            value={formatNumber(population)}
-          />
-          <DescriptionItem label="Region" value={region} />
-          <DescriptionItem label="Capital" value={capital?.join(', ')} />
-        </dl>
+
+        <DetailList label="Country details" details={countryDetails} />
       </div>
-    </div>
-  );
-}
-
-interface DetailProps {
-  label: string;
-  value: string | number | null;
-}
-
-function DescriptionItem({ label, value }: DetailProps) {
-  return (
-    <div className="flex">
-      <dt className="font-semibold">{label}:&nbsp;</dt>
-      <dd>{value ?? 'N/A'}</dd>
     </div>
   );
 }
