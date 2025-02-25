eb9aa3a32e141bce6a1953eae9c4c18e9c7d4330
Patricija Delac <patricija.delac@agiledrop.com>
2025-02-25 20:23:54 +0100

    Update NotFoundPage with a cleaner design

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,14 +1,25 @@
-import { Link } from 'react-router-dom';
+import sherlock from '../assets/images/sherlock-holmes-removed-bg.png';
+import BackLink from '../components/BackLink';
 
 export default function NotFoundPage() {
   return (
-    // TODO: Style with a more polished design.
-    <div className="flex flex-col items-center justify-center gap-4 p-4 text-center">
-      <h1>404 Not Found</h1>
-      <p>The page you're looking for does not exist.</p>
-      <Link to="/" className="mt-4 text-blue-500 hover:underline">
-        Home
-      </Link>
+    <div className="grid place-items-center gap-10 mx-auto py-10 max-w-[80%] m:grid-cols-2 m:gap-18 m:mx-none">
+      <img
+        src={sherlock}
+        alt="Detective Holmes examining the mystery of the missing page"
+        className="rounded-full border-2 border-dark-blue shadow-custom"
+      />
+
+      <div className="flex flex-col items-center justify-center gap-4 text-center m:gap-10">
+        <h1 role="alert">Oops! Page Not Found.</h1>
+
+        <p>
+          The page you're looking for must've gone undercover. Sherlock Holmes
+          is probably hot on its trail, but it keeps slipping away!
+        </p>
+
+        <BackLink />
+      </div>
     </div>
   );
 }
