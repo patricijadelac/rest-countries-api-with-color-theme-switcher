9ea438bb89e98ab29daa552123b83b9b5e256743
Patricija Delac <patricija.delac@agiledrop.com>
2025-02-26 20:00:02 +0100

    Refactor ErrorMessage component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,19 +1,17 @@
 interface ErrorMessageProps {
   title: string;
-  error: Error;
+  error?: Error;
 }
 
-const ErrorMessage = ({ title, error }: ErrorMessageProps) => {
+export default function ErrorMessage({ title, error }: ErrorMessageProps) {
   const message = error ? error.message : 'An unexpected error occurred.';
 
   return (
     <>
-      <h2>{title}</h2>
+      <h2>{title || 'Error'}</h2>
       <p className="mt-4 m:mt-8">
         <strong>Error Details:</strong> {message}
       </p>
     </>
   );
-};
-
-export default ErrorMessage;
+}
